cd0d42653748ebb39415d09ecd0dfd7af59f86e6
Thian Lim <thianlim@users.noreply.github.com>
2019-04-12 01:43:34 +0200

    Grouped search results by query.

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid, Image, Card } from "semantic-ui-react";
 import ResultList from "../searchResult/ResultList";
 import ConversationList from "../conversation/ConversationList.jsx";
 
@@ -19,13 +19,23 @@ class Dashboard extends Component {
           <Grid.Column>
             <h2>Knowledge base</h2>
             <div className="column-content">
-              <ResultList />
+              <Card fluid>
+                <Card.Content>
+                  <h5>Results for: "btw aangifte doen"</h5>
+                </Card.Content>
+                <ResultList />
+              </Card>
             </div>
           </Grid.Column>
           <Grid.Column>
             <h2>Support cases</h2>
             <div className="column-content">
-              <ResultList />
+              <Card fluid>
+                <Card.Content>
+                  <h5>Results for: "btw aangifte doen"</h5>
+                </Card.Content>
+                <ResultList />
+              </Card>
             </div>
           </Grid.Column>
         </Grid.Row>
--- src/searchResult/ResultList.jsx
+++ src/searchResult/ResultList.jsx
@@ -1,28 +1,24 @@
 import React, { Component } from "react";
-import { Card, Button } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import MockResults from "./GetSearchResult.js";
 import GetSearchResult from "./SearchResultsAPI.js";
 import "./ResultList.css";
 
 const ResultNode = props => {
   return (
-    <Card fluid>
-      <Card.Content>
-        <Card.Header>{props.result.Title}</Card.Header>
-        <Card.Meta>{props.result.Category}</Card.Meta>
-        <Card.Description>{props.result.Summary}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a
-          className="ui"
-          href={"https://support.exactonline.com/" + props.result.HRef}
-          target="_blank"
-        >
-          View solution
-        </a>
-        <a className="ui">Solved the issue</a>
-      </Card.Content>
-    </Card>
+    <Card.Content extra>
+      <Card.Header>{props.result.Title}</Card.Header>
+      <Card.Meta>{props.result.Category}</Card.Meta>
+      <Card.Description>{props.result.Summary}</Card.Description>
+      <a
+        className="ui"
+        href={"https://support.exactonline.com/" + props.result.HRef}
+        target="_blank"
+      >
+        View solution
+      </a>
+      <a className="ui">Solved the issue</a>
+    </Card.Content>
   );
 };
 
