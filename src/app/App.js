22ce41836e31884744d477c84528177eacd8f8d9
Thian Lim <thianlim@users.noreply.github.com>
2019-04-11 22:13:36 +0200

    Styles app header.

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "../dashboard/Dashboard";
 import { Image } from "semantic-ui-react";
-import ExactLogo from "../resources/exact-logo.svg";
+import ExactLogo from "../resources/exact-logo.png";
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="logo">
-            <Image src={ExactLogo} />
+            <Image src={ExactLogo} verticalAlign="middle" />
           </div>
           <div className="team-name">!Robots</div>
           <div className="team-idea">
